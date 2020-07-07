67ef0dc615bf4734cbc0b0da1cb9e1f68433e32d
Saulo Tavares <tec.saulotavares@gmail.com>
2020-07-06 23:27:22 -0300

    Desenvolvendo o backend renderizando frontend usando o nunjucks

---
+++ backend/data.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id:"sEE-3P9kKyE",
+        title:"TypeScript, Carreira & AdonisJS! ",
+        duration: "57 min",
+        price:"Free",
+        featured:true
+    },
+    {
+        id:"hN0zWNuZJC4",
+        title:"Será que eu já tive uma ideia bilionária de aplicativo?",
+        duration: "11 min",
+        price:"Free"
+    },
+    {
+        id:"rCeGfFk-uCk",
+        title:"Setup Projeto NodeJS ",
+        duration: "46 min",
+        price:"Free",
+        featured: true
+    }
+]
